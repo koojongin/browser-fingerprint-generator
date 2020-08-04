e9657da8db6839fe925c23f8e73ff035e3cf05df
koojongin <jiku90@naver.com>
2020-08-04 16:36:40 +0900

    fix: updated modulize

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const path = require('path');
 const fileSystem = require('fs');
 
 http.createServer((req, response) => {
-  let filePath = path.join(__dirname, 'index.html');
+  try {
+    let filePath = path.join(__dirname, 'index.html');
+    if(req.url == '/fingerprint-generator.js') filePath = path.join(__dirname, req.url);
 
-  if (req.url === '/') filePath = path.join(__dirname, 'index.html');
-  if (req.url === '/fingerprint.js') filePath = path.join(__dirname, '/fingerprint.js');
-
-  const readStream = fileSystem.createReadStream(filePath);
-  // We replaced all the event handlers with a simple call to readStream.pipe()
-  readStream.pipe(response);
+    const readStream = fileSystem.createReadStream(filePath);
+    readStream.pipe(response);
+  }
+  catch(e){
+    console.log(e);
+  }
 }).listen(8080);
